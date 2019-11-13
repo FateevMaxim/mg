2aad362afb2a8aa131c4289c32610c7034c47f48
Maxim Fateyev <fateev.maxim@gmail.com>
2019-11-13 11:21:04 +0600

    Some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Learning Laravel by Max
                 </div>
 
                 <div class="links">
